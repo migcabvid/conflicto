9b4cdf702a5b1f5cd7441053c5115e4f6f3c19a3
migcabvid <miguelcaballerovidal@gmail.com>
2024-10-23 09:05:35 +0200

    Confirmando combios en la rama main

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package Prueba;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World en la rama main!");
 	}
 }
