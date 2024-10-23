c638fe09b96f30ec585be222b88ed6620fe47778
migcabvid <miguelcaballerovidal@gmail.com>
2024-10-23 09:01:34 +0200

    Confirmando cambios en la rama login

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package Prueba;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hola Mundo en la rama login");
 	}
 }
